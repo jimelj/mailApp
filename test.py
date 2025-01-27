ee0a84c2699bb23e1b90c9e91238fc2cb5489072
Jimel Joseph <3507684+jimelj@users.noreply.github.com>
2025-01-27 14:21:49 -0500

    new release lets see with mac os version works

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import socket
-sock = socket.create_connection(("cba.sharefileftp.com", 990))
-print("Connected to FTPS server.")
-sock.close()
+# import socket
+# sock = socket.create_connection(("cba.sharefileftp.com", 990))
+# print("Connected to FTPS server.")
+# sock.close()
+
